a3fa1ff743c57336f6635f6862e651a77b157245
Gangadhara <gangadhar.padavala@gmail.com>
2023-11-28 08:01:44 +0530

    Added oled test project

---
+++ UART_OLEDI2c/PTR_TEST/Release/Drivers/OLED/ssd1306_fonts.d
@@ -0,0 +1,4 @@
+Drivers/OLED/ssd1306_fonts.o: ../Drivers/OLED/ssd1306_fonts.c \
+ ../Drivers/OLED/ssd1306_fonts.h ../Drivers/OLED/ssd1306_conf.h
+../Drivers/OLED/ssd1306_fonts.h:
+../Drivers/OLED/ssd1306_conf.h:
---
+++ UART_OLEDI2c/PTR_TEST/Release/Drivers/OLED/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/OLED/ssd1306.c \
+../Drivers/OLED/ssd1306_fonts.c \
+../Drivers/OLED/ssd1306_tests.c 
+
+OBJS += \
+./Drivers/OLED/ssd1306.o \
+./Drivers/OLED/ssd1306_fonts.o \
+./Drivers/OLED/ssd1306_tests.o 
+
+C_DEPS += \
+./Drivers/OLED/ssd1306.d \
+./Drivers/OLED/ssd1306_fonts.d \
+./Drivers/OLED/ssd1306_tests.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/OLED/%.o Drivers/OLED/%.su Drivers/OLED/%.cyclo: ../Drivers/OLED/%.c Drivers/OLED/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -DUSE_HAL_DRIVER -DSTM32F411xE -c -I../Core/Inc -I"C:/Users/PGR/test/PTR_TEST/Drivers/OLED" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Drivers-2f-OLED
+
+clean-Drivers-2f-OLED:
+	-$(RM) ./Drivers/OLED/ssd1306.cyclo ./Drivers/OLED/ssd1306.d ./Drivers/OLED/ssd1306.o ./Drivers/OLED/ssd1306.su ./Drivers/OLED/ssd1306_fonts.cyclo ./Drivers/OLED/ssd1306_fonts.d ./Drivers/OLED/ssd1306_fonts.o ./Drivers/OLED/ssd1306_fonts.su ./Drivers/OLED/ssd1306_tests.cyclo ./Drivers/OLED/ssd1306_tests.d ./Drivers/OLED/ssd1306_tests.o ./Drivers/OLED/ssd1306_tests.su
+
+.PHONY: clean-Drivers-2f-OLED
+
